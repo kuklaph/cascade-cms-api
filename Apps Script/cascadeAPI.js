b5c608875ef6d22d20d5525b1017fc4bd5f36693
Phil K <30103848+kuklaph@users.noreply.github.com>
2023-01-24 10:02:33 -0500

    ^JSdoc read indentation

--- Apps Script/cascadeAPI.js
+++ Apps Script/cascadeAPI.js
@@ -11,27 +11,27 @@ class CascadeAPI {
    * @property {ReadRequest_Identifier} identifier - REQUIRED: Parameters used when reading an asset
    */
   /**
-* @typedef {Object} ReadRequest_Identifier
-* @property {string} id - One is REQUIRED: id | type | path
-* - When editing and selected asset is recycled, it is recommended to preserve this relationship by providing selected asset's id in case if the selected asset gets restored from the recycle bin.
-
-* @property {ReadRequestIdentifier_Path} path - One is REQUIRED: id | type | path
-* - The Path object container.
-* - Path works only for non-recycled assets
-
-* @property {"assetfactory" | "assetfactorycontainer" | "block" | "block_FEED" | "block_INDEX" | "block_TEXT" | "block_XHTML_DATADEFINITION" | "block_XML" | "block_TWITTER_FEED" | "connectorcontainer" | "twitterconnector" | "facebookconnector" | "wordpressconnector" | "googleanalyticsconnector" | "contenttype" | "contenttypecontainer" | "destination" | "editorconfiguration" | "file" | "folder" | "group" | "message" | "metadataset" | "metadatasetcontainer" | "page" | "pageconfigurationset" | "pageconfiguration" | "pageregion" | "pageconfigurationsetcontainer" | "publishset" | "publishsetcontainer" | "reference" | "role" | "datadefinition" | "datadefinitioncontainer" | "sharedfield" | "sharedfieldcontainer" | "format" | "format_XSLT" | "format_SCRIPT" | "site" | "sitedestinationcontainer" | "symlink" | "target" | "template" | "transport" | "transport_fs" | "transport_ftp" | "transport_db" | "transport_cloud" | "transportcontainer" | "user" | "workflow" | "workflowdefinition" | "workflowdefinitioncontainer" | "workflowemail" | "workflowemailcontainer"} type - One is REQUIRED: id | type | path
-* - The type of asset to read.
-
-* @property {boolean} [recycled] - NOT REQUIRED: For reading purposes only. Ignored when editing, copying etc.
-*/
-  /**
-* @typedef {Object} ReadRequestIdentifier_Path
-* @property {string} [path] - NOT REQUIRED: The path to the asset.
-* - When reading a site, the 'path' element should be populated with the parent site's name
-
-* @property {string} [siteId] - NOT REQUIRED: The siteId of the parent site.
-* @property {string} [siteName] - NOT REQUIRED: The parent siteName
-*/
+   * @typedef {Object} ReadRequest_Identifier
+   * @property {string} id - One is REQUIRED: id | type | path
+   * - When editing and selected asset is recycled, it is recommended to preserve this relationship by providing selected asset's id in case if the selected asset gets restored from the recycle bin.
+   
+   * @property {ReadRequestIdentifier_Path} path - One is REQUIRED: id | type | path
+   * - The Path object container.
+   * - Path works only for non-recycled assets
+   
+   * @property {"assetfactory" | "assetfactorycontainer" | "block" | "block_FEED" | "block_INDEX" | "block_TEXT" | "block_XHTML_DATADEFINITION" | "block_XML" | "block_TWITTER_FEED" | "connectorcontainer" | "twitterconnector" | "facebookconnector" | "wordpressconnector" | "googleanalyticsconnector" | "contenttype" | "contenttypecontainer" | "destination" | "editorconfiguration" | "file" | "folder" | "group" | "message" | "metadataset" | "metadatasetcontainer" | "page" | "pageconfigurationset" | "pageconfiguration" | "pageregion" | "pageconfigurationsetcontainer" | "publishset" | "publishsetcontainer" | "reference" | "role" | "datadefinition" | "datadefinitioncontainer" | "sharedfield" | "sharedfieldcontainer" | "format" | "format_XSLT" | "format_SCRIPT" | "site" | "sitedestinationcontainer" | "symlink" | "target" | "template" | "transport" | "transport_fs" | "transport_ftp" | "transport_db" | "transport_cloud" | "transportcontainer" | "user" | "workflow" | "workflowdefinition" | "workflowdefinitioncontainer" | "workflowemail" | "workflowemailcontainer"} type - One is REQUIRED: id | type | path
+   * - The type of asset to read.
+   
+   * @property {boolean} [recycled] - NOT REQUIRED: For reading purposes only. Ignored when editing, copying etc.
+   */
+   /**
+   * @typedef {Object} ReadRequestIdentifier_Path
+   * @property {string} [path] - NOT REQUIRED: The path to the asset.
+   * - When reading a site, the 'path' element should be populated with the parent site's name
+   
+   * @property {string} [siteId] - NOT REQUIRED: The siteId of the parent site.
+   * @property {string} [siteName] - NOT REQUIRED: The parent siteName
+  */
   //#endregion
 
   // ─── Read Response ───────────────────────────────────────────────────────────
