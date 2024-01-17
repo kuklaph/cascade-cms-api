b49e4ff14e450de6ba7dc7164b2dc86b98906f0a
Philip Kukla <30103848+kuklaph@users.noreply.github.com>
2024-01-17 11:55:08 -0500

    Moved export

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as Requests from "./types.js";
-
+export * as Types from "./types.js";
 export function CascadeAPI({ apiKey, url }) {
   const timeout = 5000;
   const withAxios = async (requestParams) => {
@@ -333,5 +333,3 @@ export function CascadeAPI({ apiKey, url }) {
     },
   };
 }
-
-export * as Types from "./types.js";
